f0706b4885c4b9f72f7a15e97e33741cd30a407d
cyberchinna <jerrellroberson@yahoo.com>
2020-05-21 10:19:09 -0400

    Updated Project Assignments

---
+++ web_fundamental/javascript_predict_the_output_arrays/predict_the_output_arrays.js
@@ -0,0 +1,40 @@
+//Predict 1
+//Prediction: 8,6,7,5,3,0,9
+var arr = [8,6,7,5,3,0,9]
+for(var i = 0; i < arr.length; i++){    
+    console.log(arr[i]);
+}
+
+
+//Predict 2
+//Prediction: odd,odd,8,4,2,0,odd
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
+
+//Predict 3
+//Prediction [arr]: 1,3,8,-5,0,-2,4,-1
+//Prediction [arr mod]: -1,-3,-8,5,Zero,2,-4,1
+//Prediction [newArr]: -5,-2,-1
+var arr = [1,3,8,-5,0,-2,4,-1];
+var newArr = [];
+for(var i = 0; i< arr.length; i++){
+    if(arr[i] < 0){
+        newArr.push(arr[i]);
+        arr[i] = arr[i] * -1;
+    }
+    else if(arr[i] == 0){
+        arr[i] = "Zero";
+    }
+    else{
+        arr[i] = arr[i] * -1;
+    }
+}
+console.log(arr);
+console.log(newArr);
